d69c3f78b3eb38bff9e8dd6c09d1dde1194a2daa
chrom-P <kkllmm104@gmail.com>
2022-02-12 20:41:04 +0900

    ?

--- least-squares-method.c
+++ least-squares-method.c
@@ -11,7 +11,7 @@ int main(void)
     float x[N], y[N], a, b, sx=0.0, sy=0.0, sx2=0.0, sxy=0.0;
     FILE *fp;
 
-    if ((fp=fopen("20211125-1.dat","r"))==NULL){
+    if ((fp=fopen("text","r"))==NULL){
         printf("cannot open file/n");
         exit(0);
     }
@@ -32,5 +32,15 @@ int main(void)
     printf("y=%.4f",b);
     if(b>0) printf("+");
     printf("%.4f %.4f(%4.f)/n",x[i],y[i],a+b*x[i]);
-    
+    for(i=0;i<n;i++)
+    printf("%.4f %.4f(%.4f) ε = %.4f\n",x[i],y[i],a+b*x[i],y[i]-a-b*x[i]);
+
 }
+
+/*
+y=0.9773-0.0225x
+-1.0000 1.0000(0.9998) ε＝0.0002
+-0.1000 1.0990(0.9796) ε＝0.1194
+0.2000 0.8080(0.9728) ε＝-0.1648
+1.0000 1.0000(0.9549) ε＝0.0451
+*/
